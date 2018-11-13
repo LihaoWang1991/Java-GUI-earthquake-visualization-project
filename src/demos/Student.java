a2e2b1711d0c46475b0f1591a693ad1121d5a557
Lihao WANG <py_wanglh@163.com>
2018-11-13 23:45:07 +0100

    Add files via upload

---
+++ src/demos/Student.java
@@ -0,0 +1,29 @@
+package demos;
+
+/** A class to store information about a Student
+ *  Used in module 4 of the UC San Diego MOOC Object Oriented Programming in Java
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * 
+ */
+public class Student extends Person
+{
+	public Student(String name)  {
+		super(name);
+	}
+
+	public boolean isAsleep( int hr ) // override 
+	{ 
+		return 2 < hr && 8 > hr; 
+	}
+	
+	public static void main(String[] args)
+	{
+		Person p;
+		p = new Student("Sally");
+		p.status(1);
+		Person q= new Person("Henry");
+		Student s= new Student("Bob");
+		s=q;
+	}
+}
